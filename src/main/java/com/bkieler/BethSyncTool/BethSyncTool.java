1da25d2300ffa948831ee666377908a5fc147fce
B. Kieler <bakieler@gmail.com>
2025-04-15 20:59:33 -0500

    Base of project. No code.

---
+++ src/main/java/com/bkieler/BethSyncTool/BethSyncTool.java
@@ -0,0 +1,13 @@
+package com.bkieler.BethSyncTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BethSyncTool {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BethSyncTool.class, args);
+	}
+
+}
---
+++ src/test/java/com/bkieler/BethSyncTool/BethSyncToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkieler.BethSyncTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BethSyncToolTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
